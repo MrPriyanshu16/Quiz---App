ce591ad0c3114fe794143bc88552ae4654903382
MrPriyanshu16 <prajapatpriyanshu03@gmail.com>
2026-02-17 14:32:28 +0530

    Implementing new components and starting with components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import logo from '../assets/quiz-logo.png'
+export default function Header(){
+    return <header>
+        <img src = {logo} alt = 'Quiz - App'/>
+        <h1>Quiz App</h1>
+    </header>
+}
